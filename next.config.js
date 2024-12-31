28fccc88f704f91632114d2be769b4789c662019
Björn Kenneth Holmström <bjorn.kenneth.holmstrom@gmail.com>
2024-12-31 02:00:02 +0100

    Fix deployment on GitHub pages

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@ const withNextIntl = require('next-intl/plugin')();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export'
+  output: 'export',
+  basePath: '/peace', // replace 'peace' with your repository name
+  images: {
+    unoptimized: true
+  }
 };
 
 module.exports = withNextIntl(nextConfig);
